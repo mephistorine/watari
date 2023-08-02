d3246f78e9b64bf797026742a7cb93ff20740619
Sam Bulatov <mephistorine@gmail.com>
2023-08-02 22:33:06 +0300

    Add TaigaUI

--- apps/watari/src/app/app.component.ts
+++ apps/watari/src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify"
+import { TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule } from "@taiga-ui/core"
 import { Component } from "@angular/core"
 import { RouterModule } from "@angular/router"
 
 @Component({
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
   selector: "watari-root",
   templateUrl: "./app.component.html",
-  styleUrls: [ "./app.component.css" ]
-})
+  styleUrls: [ "./app.component.css" ],
+        providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+    })
 export class AppComponent {
 }
--- apps/watari/src/app/app.config.ts
+++ apps/watari/src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig } from "@angular/core"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { TuiRootModule } from "@taiga-ui/core"
+import { ApplicationConfig, importProvidersFrom } from "@angular/core"
 import { provideRouter, withEnabledBlockingInitialNavigation } from "@angular/router"
+
 import { appRoutes } from "./app.routes"
 
 export const appConfig: ApplicationConfig = {
-  providers: [ provideRouter(appRoutes, withEnabledBlockingInitialNavigation()) ]
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(TuiRootModule, BrowserAnimationsModule)
+  ]
 }
